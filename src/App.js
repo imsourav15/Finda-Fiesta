687af52b645dbb5d038a7d4faced4a1d23064d31
imsourav15 <sourav.shandilya@juspay.in>
2023-11-22 14:27:13 +0530

    find-User-Based-On-UserName

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [uName, setUName] = useState('');
+  const [githubUsers, setGithubUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  const findUsers = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/search/users?q=${uName}&sort=followers`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch users Details!');
+      }
+      const data = await response.json();
+      setGithubUsers(data.items);
+      setError(null);
+    } catch (error) {
+      console.error('💀 Error 404 Not Found 💀.', error);
+      setError('💀 404 Not Found 💀.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Finda Fiesta</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          value={uName}
+          onChange={(e) => setUName(e.target.value)}
+        />
+        <button onClick={findUsers}>Search</button>
+      </div>
+
+      {error && <p className="error-message">{error}</p>}
+
+      <div className="output">
+        <table>
+          <thead>
+            <tr>
+              <th>GitHub Avatar</th>
+              <th>GitHub Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            {githubUsers.map((user) => (
+              <tr key={user.id}>
+                <td>
+                  <img src = {user.avatar_url} alt = "Avatar" width = "150" height = "100" />
+                </td>
+                <td>{user.login}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
